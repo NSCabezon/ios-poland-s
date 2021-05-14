33b148dd058a260a2a191f86df39e02b2dcb98c0
Rodrigo Jurado <rjurado@ext.vectoritcgroup.com>
2021-05-14 10:34:15 +0200

    ADD: Common GP balance DTO.

---
+++ Modules/Libraries/SANPLLibrary/SANPLLibrary/BSANServices/Global Position/DTO/BalanceDTO.swift
@@ -0,0 +1,13 @@
+//
+//  BalanceDTO.swift
+//  SANPLLibrary
+//
+//  Created by Rodrigo Jurado on 14/5/21.
+//
+
+import Foundation
+
+public struct BalanceDTO: Codable {
+    public let value: Double
+    public let currencyCode: String
+}
