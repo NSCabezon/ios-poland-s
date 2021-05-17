1394ecb04ad04141f463042d4f09a162574f7291
Ernesto Fernández Calles <ernesto.fernandez@hcd.es>
2021-05-17 16:36:40 +0200

    NEW: Added home modiffiers (#359)

---
+++ Project/Santander/Configuration/Modifiers/PLCardHomeActionModifier.swift
@@ -0,0 +1,13 @@
+//
+//  PLCardHomeActionModifier.swift
+//  Santander
+//
+//  Created by Ernesto Fernandez Calles on 17/5/21.
+//
+
+import Foundation
+import Cards
+
+final class PLCardHomeActionModifier: CardHomeActionModifier {
+    
+}
---
+++ Project/Santander/Configuration/Modifiers/PLTransferHomeActionModifier.swift
@@ -0,0 +1,13 @@
+//
+//  PLTransferHomeActionModifier.swift
+//  Santander
+//
+//  Created by Ernesto Fernandez Calles on 17/5/21.
+//
+
+import Foundation
+import Transfer
+
+final class PLTransferHomeActionModifier: TransferHomeActionModifier {
+    
+}
