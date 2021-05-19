53fd429db75116e19bc3a8c76fa82ac50c96ae63
Rodrigo Jurado <rjurado@ext.vectoritcgroup.com>
2021-05-19 14:37:12 +0200

    NEW: add deposits DTO (#735)

---
+++ Modules/Libraries/SANPLLibrary/SANPLLibrary/BSANServices/Global Position/DTO/Deposits/DepositDTO.swift
@@ -0,0 +1,29 @@
+//
+//  DepositDTO.swift
+//  SANPLLibrary
+//
+//  Created by Rodrigo Jurado on 19/5/21.
+//
+
+import Foundation
+
+public struct DepositDTO: Codable {
+    public let number: String?
+    public let name: DepositNameDTO?
+    public let accountId: DepositIdDTO?
+    public let type: String?
+    public let currentBalance: BalanceDTO?
+    public let currencyCode: String?
+    public let role: String?
+    public let lastUpdate: String?
+}
+
+public struct DepositNameDTO: Codable {
+    public let userDefined: String?
+    public let description: String?
+}
+
+public struct DepositIdDTO: Codable {
+    public let id: String?
+    public let systemId: Int?
+}
