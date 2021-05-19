ab196d717b6693d8cb764f8587e62577843d48d6
Rodrigo Jurado <rjurado@ext.vectoritcgroup.com>
2021-05-19 11:03:56 +0200

    ADD: add global position DTO adapter and add cards adapter call in it (#383)

---
+++ Modules/Libraries/PLLegacyAdapter/PLLegacyAdapter/GlobalPosition/GlobalPosition/GlobalPositionDTOAdapter.swift
@@ -0,0 +1,20 @@
+//
+//  GlobalPositionDTOAdapter.swift
+//  PLLegacyAdapter
+//
+
+import SANPLLibrary
+import SANLegacyLibrary
+
+final class GlobalPositionDTOAdapter {
+    func adaptPLGlobalPositionToGlobalPosition(_ plGlobalPosition: SANPLLibrary.GlobalPositionDTO) -> SANLegacyLibrary.GlobalPositionDTO {
+        var globalPositionDTO = SANLegacyLibrary.GlobalPositionDTO()
+        let cards = plGlobalPosition.cards?.compactMap({ card -> SANLegacyLibrary.CardDTO? in
+            let cardDTOAdapter = CardDTOAdapter()
+            return cardDTOAdapter.adaptPLCardToCardDTO(card)
+        })
+        globalPositionDTO.cards = cards
+
+        return globalPositionDTO
+    }
+}
