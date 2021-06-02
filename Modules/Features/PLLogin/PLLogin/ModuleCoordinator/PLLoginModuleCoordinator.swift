19e0198ab82a019a2af56361fe2b71384c0c8993
Israel Marcos Álvarez Mesa <marcos.alvarez@experis.es>
2021-06-02 15:06:24 +0100

    Several changes in Login process screens (Code review) (MOVPL-393)

--- Modules/Features/PLLogin/PLLogin/ModuleCoordinator/PLLoginModuleCoordinator.swift
+++ Modules/Features/PLLogin/PLLogin/ModuleCoordinator/PLLoginModuleCoordinator.swift
@@ -6,11 +6,17 @@ public class PLLoginModuleCoordinator: ModuleSectionedCoordinator {
     public weak var navigationController: UINavigationController?
     private let dependenciesEngine: DependenciesDefault
     private let unrememberdLoginIdCoordinator: PLUnrememberedLoginIdCoordinator
+    //REMOVE THIS BEFORE MERGING
+    private let unrememberedLoginMaskedPawdCoordinator: PLUnrememberedLoginMaskedPwdCoordinator
+    private let unrememberedLoginNormalPawdCoordinator: PLUnrememberedLoginNormalPwdCoordinator
     
     public init(dependenciesResolver: DependenciesResolver, navigationController: UINavigationController?) {
         self.navigationController = navigationController
         self.dependenciesEngine = DependenciesDefault(father: dependenciesResolver)
         self.unrememberdLoginIdCoordinator = PLUnrememberedLoginIdCoordinator(dependenciesResolver: dependenciesEngine, navigationController: navigationController)
+        //REMOVE THIS BEFORE MERGING
+        self.unrememberedLoginMaskedPawdCoordinator = PLUnrememberedLoginMaskedPwdCoordinator(dependenciesResolver: dependenciesResolver, navigationController: navigationController)
+        self.unrememberedLoginNormalPawdCoordinator = PLUnrememberedLoginNormalPwdCoordinator(dependenciesResolver: dependenciesResolver, navigationController: navigationController)
     }
 }
 
@@ -19,6 +25,9 @@ extension PLLoginModuleCoordinator: LoginModuleCoordinatorProtocol {
         switch section {
         case .unrememberedLogin:
             return self.unrememberdLoginIdCoordinator.start()
+        //REMOVE THIS BEFORE MERGING
+//            return unrememberedLoginMaskedPawdCoordinator.start()
+//            return unrememberedLoginNormalPawdCoordinator.start()
         case .loginRemembered:
             break
         case .quickBalance:
