2d73e4ff8bcac4d55f20cdec2149f94f619d41b0
Rodrigo Jurado <rjurado@ext.vectoritcgroup.com>
2021-06-10 13:25:03 +0200

    NEW: add onboarding images for the GP styles (#210)

---
+++ Modules/Features/PLLogin/PLLogin/LoginScenes/UseCases/GlobalPositionOptionUseCase.swift
@@ -0,0 +1,10 @@
+//
+//  GlobalPositionOptionUseCase.swift
+//  PLLogin
+//
+//  Created by Rodrigo Jurado on 10/6/21.
+//
+
+import Foundation
+
+final class GetGlobalPositionOptionUseCase: UseCase<Void, GetGlobalPositionOptionUseCaseOkOutput, StringErrorOutput> {
