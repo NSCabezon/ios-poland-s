cd698617049d4e0a650948dd152e90993c97fff3
Jose C. Yebes <jose.yebes@ciberexperis.es>
2021-08-03 15:38:27 +0200

    UPDATE: Compatibility with core naming change

--- Modules/Libraries/PLLegacyAdapter/PLLegacyAdapter/ManagerAdapter/PLSubscriptionManagerAdapter.swift
+++ Modules/Libraries/PLLegacyAdapter/PLLegacyAdapter/ManagerAdapter/PLSubscriptionManagerAdapter.swift
@@ -7,11 +7,11 @@ import SANLegacyLibrary
 final class PLSubscriptionManagerAdapter {}
 
 extension PLSubscriptionManagerAdapter: BSANSubscriptionManager {
-    func activate(token: String, instaID: String) throws -> BSANResponse<Void> {
+    func activate(magicPhrase token: String, instaID: String) throws -> BSANResponse<Void> {
         return BSANErrorResponse(nil)
     }
 
-    func deactivate(token: String, instaID: String) throws -> BSANResponse<Void> {
+    func deactivate(magicPhrase token: String, instaID: String) throws -> BSANResponse<Void> {
         return BSANErrorResponse(nil)
     }
 }
