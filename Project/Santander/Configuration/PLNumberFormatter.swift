f6809225a5f57f4e2fd7faa9db14443e70e278c6
Francisco Pérez Martínez <85613318+francisco-perez-m@users.noreply.github.com>
2021-09-24 12:07:30 +0200

    FIX: Implement currency and format changes (#1540) (#173)

--- Project/Santander/Configuration/PLNumberFormatter.swift
+++ Project/Santander/Configuration/PLNumberFormatter.swift
@@ -8,9 +8,13 @@ import SANLegacyLibrary
 final class PLNumberFormatter: AmountFormatterProvider {
     func formatterForRepresentation(_ amountRepresentation: AmountRepresentation) -> NumberFormatter? {
         let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
+        numberFormatter.groupingSeparator = " "
+        numberFormatter.maximumFractionDigits = 2
         numberFormatter.minimumFractionDigits = 2
-        numberFormatter.locale = Locale(identifier: "pl_PL")
+        numberFormatter.maximumIntegerDigits = 12
+        numberFormatter.minimumIntegerDigits = 1
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.locale = Locale(identifier: "de")
         return numberFormatter
     }
 }
@@ -25,6 +29,24 @@ extension PLNumberFormatter: CurrencyFormatterProvider {
     }
 
     func assembleCurrencyString(for value: String, with symbol: String, representation: AmountRepresentation) -> CurrencySymbolPosition {
-        return .rightPadded
+        let formattedSymbol = self.getFormattedCurrency(for: symbol)
+        return .custom(formattedString: "\(value) \(formattedSymbol)")
+    }
+}
+
+private extension PLNumberFormatter {
+    private func getFormattedCurrency(for symbol: String) -> String {
+        switch symbol {
+        case "€":
+            return "EUR"
+        case "$":
+            return "USD"
+        case "£":
+            return "GBP"
+        case "PLN":
+            return "PLN"
+        default:
+            return symbol.uppercased()
+        }
     }
 }
