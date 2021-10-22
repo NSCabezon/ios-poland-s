1cf800fe91f74ced0fad84ce86f910d8caaebf69
Marcos Álvarez <84910843+MarcosAlvarezExp@users.noreply.github.com>
2021-10-22 16:02:03 +0100

    Missing file (MOVPL-1740) (#294)
    * MOVPL-1229
    
    * FIX: Fixed problem in PLUIInputCodeView not going to previous box when deleting the last element (MOVPL-1229)
    
    * FIX: Fixed warnings (MOVPL-1229)
    
    * Fixed warnings (MOVPL-1740)
    
    * Removed commented import (MOVPL-1740)
    
    * Cryptography helper and Unit Tests (MOVPL-1740)
    
    * Created use cases and unit tests form the use of cryptograhy (MOVPL-1740)
    
    * Created use cases and unit tests form the use of cryptograhy (MOVPL-1740)
    
    * Created use cases for storing and getting encrypted user keys (MOVPL-1740)
    
    * Final integration with cryptography in the flow (MOVPL-1740)
    
    * Store certificate in keychain integration and refactor of use cases (MOVPL-1740)
    
    * Created extension for SecIdentity (MOVPL-1740)
    
    * Changed mapped type (MOVPL-1740)
    
    * Moved Constants folder (MOVPL-1740)

---
+++ Modules/Features/PLLogin/PLLogin/Constants/PLLoginConstants.swift
@@ -0,0 +1,10 @@
+//
+//  PLLoginConstants.swift
+//  PLLogin
+//
+//  Created by Marcos Álvarez Mesa on 22/10/21.
+//
+
+public enum PLLoginConstants {
+    public static let certificateIdentityLabel = "PLCertificateIdentity"
+}
