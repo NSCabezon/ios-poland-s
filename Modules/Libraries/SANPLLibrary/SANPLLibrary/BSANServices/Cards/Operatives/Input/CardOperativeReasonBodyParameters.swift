2631cb08bf17ef4bcfebab4bacdb9d936c5b3e15
Ernesto Fernández Calles <85167653+hcdErnestoFC@users.noreply.github.com>
2021-11-03 11:29:01 +0100

    NEW: Updated network calls (#1759) (#346)

---
+++ Modules/Libraries/SANPLLibrary/SANPLLibrary/BSANServices/Cards/Operatives/Input/CardOperativeReasonBodyParameters.swift
@@ -0,0 +1,12 @@
+//
+//  CardDisableParameters.swift
+//  SANPLLibrary
+//
+//  Created by Ernesto Fernandez Calles on 11/10/21.
+//
+
+import Foundation
+
+public struct CardOperativeReasonBodyParameters: Encodable {
+    public let reason: String
+}
