4c02f9a75a8e664122354949f526e72af478179f
Fernando Sanchez <85502830+fernando-sanchez-garciamadrid@users.noreply.github.com>
2021-11-08 18:03:43 +0100

    FIX: load correct fields interestType and currentInterestAmount (#378)
    Co-authored-by: FERNANDO SANCHEZ GARCIA-MADRID <x478659@gruposantander.es>

--- Modules/Libraries/PLLegacyAdapter/PLLegacyAdapter/Loan/Details/LoanDetailsDTOAdapter.swift
+++ Modules/Libraries/PLLegacyAdapter/PLLegacyAdapter/Loan/Details/LoanDetailsDTOAdapter.swift
@@ -12,13 +12,14 @@ final class LoanDetailsDTOAdapter {
     static func adaptPLLoanDetailsToLoanDetails(_ plLoanDetails: SANPLLibrary.LoanDetailDTO, plLoanInstallments: SANPLLibrary.LoanInstallmentsListDTO?) -> SANLegacyLibrary.LoanDetailDTO {
         var loanDetailDTO = SANLegacyLibrary.LoanDetailDTO()
         loanDetailDTO.openingDate = DateFormats.toDate(string: plLoanDetails.accountDetails?.openedDate ?? "", output: .YYYYMMDD)
-        let interestType = String(plLoanDetails.loanAccountDetails?.interest?.rate ?? 0) + "%"
+        let interestType = String(plLoanDetails.accountDetails?.interestRate ?? 0) + "%"
         loanDetailDTO.interestType = interestType.replacingOccurrences(of: ".", with: ",", options: .literal, range: nil)
         loanDetailDTO.initialAmount = AmountAdapter.adaptBalanceToAmount(plLoanDetails.loanAccountDetails?.grantedCreditLimit)
         loanDetailDTO.nextInstallmentDate = DateFormats.toDate(string: plLoanDetails.loanAccountDetails?.nextInstallmentDate ?? "", output: .YYYYMMDD)
         loanDetailDTO.currentDueDate = DateFormats.toDate(string: plLoanDetails.loanAccountDetails?.finalRepaymentDate ?? "", output: .YYYYMMDD)
-        if let currentInterestAmount = plLoanInstallments?.installments?.last?.totalPayment {
-            loanDetailDTO.currentInterestAmount = "\(currentInterestAmount)"
+        if let currentInterestAmount = plLoanDetails.loanAccountDetails?.interest?.previousTotalAmount?.value {
+            let currentInterestAmountSt = "\(currentInterestAmount)"
+            loanDetailDTO.currentInterestAmount = currentInterestAmountSt.replacingOccurrences(of: ".", with: ",", options: .literal, range: nil)
         }
 
         return loanDetailDTO
