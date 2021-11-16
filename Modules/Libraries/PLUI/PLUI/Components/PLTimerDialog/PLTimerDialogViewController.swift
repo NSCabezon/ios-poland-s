9ab773a54fbbe2d30dc8cce4f73c3860ec4231a4
Mario Rosales Maillo <85669754+MarioRosalesMaillo@users.noreply.github.com>
2021-11-16 12:27:29 +0100

    Fix: recover corret logic for timer dialog (movpl-2213) (#415)
    * FIX: Fix timer dialog end calculation (MOVPL-2213)
    
    * FIX: refactor code and recover old implementation (MOVPL-2213)

--- Modules/Libraries/PLUI/PLUI/Components/PLTimerDialog/PLTimerDialogViewController.swift
+++ Modules/Libraries/PLUI/PLUI/Components/PLTimerDialog/PLTimerDialogViewController.swift
@@ -64,9 +64,9 @@ class PLTimerDialogViewController: UIViewController {
     // MARK: - Private
     
     @objc private func updateText() {
-        if self.dateTimeStamp > 0 {
-            let date = Date(timeIntervalSince1970: self.dateTimeStamp)
-            guard date > now else { return }
+        let date = Date(timeIntervalSince1970: self.dateTimeStamp)
+        if date > now {
+    
             let formatter = DateComponentsFormatter()
             formatter.allowedUnits = [.hour, .minute, .second]
 
