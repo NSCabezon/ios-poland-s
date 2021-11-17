5a772a3e0919c1b8cb0f5d0cfd1717b295ddc764
Jose C. Yebes <jose.yebes@ciberexperis.es>
2021-11-17 20:44:04 +0100

    FIX: Temporal fix for remembered login

--- Project/Santander/Modifiers/Session/PLSessionDataManagerModifier.swift
+++ Project/Santander/Modifiers/Session/PLSessionDataManagerModifier.swift
@@ -8,6 +8,7 @@
 import Commons
 import DomainCommon
 import Models
+import Repository
 
 final class PLSessionDataManagerModifier: SessionDataManagerModifier {
     private let dependenciesResolver: DependenciesResolver
@@ -17,6 +18,15 @@ final class PLSessionDataManagerModifier: SessionDataManagerModifier {
     }
     
     func performAfterGlobalPosition(_ globalPosition: GlobalPositionRepresentable) -> ScenarioHandler<Void, StringErrorOutput>? {
+
+        // TODO: Remove this when complete implementation available, this only fix the remembered login navigation
+        let appRepository = self.dependenciesResolver.resolve(for: AppRepositoryProtocol.self)
+        guard let userDto = try? appRepository.getPersistedUser().getResponseData() else { return nil }
+        userDto.name = globalPosition.fullName != "" ? globalPosition.fullName : globalPosition.clientNameWithoutSurname
+        userDto.userId = globalPosition.userId
+        _ = appRepository.setPersistedUserDTO(persistedUserDTO: userDto)
+        // ----------
+
         return nil
     }
     
