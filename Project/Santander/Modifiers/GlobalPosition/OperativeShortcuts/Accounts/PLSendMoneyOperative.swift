bc6bfb1d1e4676bb40da472a20d765ffad643d07
Alvaro Royo <alvaroroyo3@gmail.com>
2021-11-30 16:56:44 +0100

    Send money in shorcuts (#539)

--- Project/Santander/Modifiers/GlobalPosition/OperativeShortcuts/Accounts/PLSendMoneyOperative.swift
+++ Project/Santander/Modifiers/GlobalPosition/OperativeShortcuts/Accounts/PLSendMoneyOperative.swift
@@ -6,14 +6,20 @@
 import Models
 import UI
 import Commons
+import UIKit
 
 final class PLSendMoneyOperative: AccountOperativeActionTypeProtocol {
+    let dependenciesResolver: DependenciesResolver
     var rawValue: String = "accountOptionButtonTransferPoland"
     var trackName: String? = "accountOptionButtonTransferPoland"
     var accessibilityIdentifier: String? = "accountOptionButtonTransfer"
     private let title: String = "accountOption_button_transfer"
     private let icon: String = "icnSendMoney"
 
+    init(dependenciesResolver: DependenciesResolver) {
+        self.dependenciesResolver = dependenciesResolver
+    }
+    
     func values() -> (title: String, imageName: String) {
         return (self.title, self.icon)
     }
@@ -27,7 +33,18 @@ final class PLSendMoneyOperative: AccountOperativeActionTypeProtocol {
 
     func getAction() -> AccountOperativeAction {
         return .custom {
-            Toast.show(localized("generic_alert_notAvailableOperation"))
+            let useCase = CheckNewSendMoneyEnabledUseCase(dependenciesResolver: self.dependenciesResolver)
+            Scenario(useCase: useCase)
+                .execute(on: self.dependenciesResolver.resolve())
+                .onSuccess { [weak self] enabled in
+                    if enabled {
+                        UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true, completion: {
+                            self?.dependenciesResolver.resolve(for: SendMoneyCoordinatorProtocol.self).start()
+                        })
+                    } else {
+                        Toast.show(localized("generic_alert_notAvailableOperation"))
+                    }
+                }
         }
     }
 }
