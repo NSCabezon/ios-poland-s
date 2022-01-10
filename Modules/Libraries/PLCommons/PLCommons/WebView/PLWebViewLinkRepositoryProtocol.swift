68647cf99e2afe782c5e817ebca1d52249100f2c
Aleksy Tylkowski <94537359+aleksy-t@users.noreply.github.com>
2022-01-10 10:33:45 +0100

    Feature/mobile 9734 add write to us button in help desk (#661)
    * added icon with header
    
    * added entry
    
    * Update .gitmodules
    
    * Update .gitmodules

--- Modules/Libraries/PLCommons/PLCommons/WebView/PLWebViewLinkRepositoryProtocol.swift
+++ Modules/Libraries/PLCommons/PLCommons/WebView/PLWebViewLinkRepositoryProtocol.swift
@@ -23,6 +23,7 @@ public struct PLWebViewLink {
 public enum PLWebViewLinkRepositoryGroup {
     case accounts
     case cards
+    case helpCenter
 }
 
 public protocol PLWebViewLinkRepositoryProtocol {
--- Project/Santander/Modifiers/Accounts/OtherOperatives/DTO/PLAccountOtherOperativesDTOList.swift
+++ Project/Santander/Modifiers/Accounts/OtherOperatives/DTO/PLAccountOtherOperativesDTOList.swift
@@ -8,10 +8,11 @@
 import Foundation
 
 struct PLAccountOtherOperativesDTOList: Codable {
-    var accountsOptions, cardsOptions: [PLAccountOtherOperativesDTO]?
+    var accountsOptions, cardsOptions, helpCenterOptions: [PLAccountOtherOperativesDTO]?
 
     enum CodingKeys: String, CodingKey {
         case accountsOptions = "accounts_options"
         case cardsOptions = "cards_options"
+        case helpCenterOptions = "help_center_options"
     }
 }
