bb128cf2154b939202ece425414400941f6e3285
Juan Jose Acosta Gonzalez <juan.jose.acosta@experis.es>
2022-02-07 12:48:59 +0100

    WIP: added ids

--- Project/Santander/Dependencies/Modules/Menu/PublicMenu/Modifiers/PLKindOfPublicMenuNode.swift
+++ Project/Santander/Dependencies/Modules/Menu/PublicMenu/Modifiers/PLKindOfPublicMenuNode.swift
@@ -7,11 +7,11 @@
 import CoreFoundationLib
 
 extension KindOfPublicMenuNode {
-    static let otherUserViewModel = (titleKey: "pl_menuPublic_link_changeUser", iconKey: "icnChangeUser", accessibility: "btnOtherUser")
-    static let informationViewModel = (titleKey: "pl_menuPublic_link_information", iconKey: "icnInfoRedLight", accessibility: "btnInfo")
-    static let serviceViewModel = (titleKey: "pl_menuPublic_link_services", iconKey: "icnMcommerce", accessibility: "btnService")
-    static let plATMViewModel = (titleKey: "menuPublic_link_checkAtm", iconKey: "icnMapPointSan", accessibility: "btnAtm")
-    static let offerViewModel = (titleKey: "menuPublic_link_becomeClient", iconKey: "icnOffer", accessibility: "btnOffer")
-    static let contactViewModel = (titleKey: "pl_menuPublic_link_emergency", iconKey: "icnPhoneRed", accessibility: "btnContact")
-    static let mobileAuthorizationViewModel = (titleKey: "pl_menuPublic_link_mobileAuthorization", iconKey: "icnMobileAuthorization", accessibility: "btnMobileAuthorization")
+    static let otherUserViewModel = (titleKey: "pl_menuPublic_link_changeUser", iconKey: "icnChangeUser", accessibility: "publicMenuChangeUser")
+    static let informationViewModel = (titleKey: "pl_menuPublic_link_information", iconKey: "icnInfoRedLight", accessibility: "publicMenuInformation")
+    static let serviceViewModel = (titleKey: "pl_menuPublic_link_services", iconKey: "icnMcommerce", accessibility: "publicMenuServices")
+    static let plATMViewModel = (titleKey: "menuPublic_link_checkAtm", iconKey: "icnMapPointSan", accessibility: "publicMenuNearbyATM")
+    static let offerViewModel = (titleKey: "menuPublic_link_becomeClient", iconKey: "icnOffer", accessibility: "publicMenuOurOffer")
+    static let contactViewModel = (titleKey: "pl_menuPublic_link_emergency", iconKey: "icnPhoneRed", accessibility: "publicMenuContact")
+    static let mobileAuthorizationViewModel = (titleKey: "pl_menuPublic_link_mobileAuthorization", iconKey: "icnMobileAuthorization", accessibility: "publicMenuMobileAuthorization")
 }
