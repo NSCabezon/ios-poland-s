e3d16635f48a032b9414fd040d0b2ca2276bef20
Dawid Czmyr <96076453+dczmyr@users.noreply.github.com>
2022-02-24 15:02:38 +0100

    Add PLPublicMenuHelpCenterCoordinator to show help center before login (#881)
    * Add PLPublicMenuHelpCenterCoordinator to show help center before login
    
    * Added PR reviews comments fix
    
    Co-authored-by: Dawid <188454@santander.pl>
    Co-authored-by: Dawid Ogonowski <>

---
+++ Modules/Features/PLHelpCenter/PLHelpCenter/ModuleCoordinator/PLPublicMenuHelpCenterCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  PLPublicMenuHelpCenterCoordinator.swift
+//  PLHelpCenter
+//
+//  Created by 188454 on 22/02/2022.
+//
+import UI
+import CoreFoundationLib
+
+public final class PLPublicMenuHelpCenterCoordinator: BindableCoordinator {
+    private let dependenciesResolver: DependenciesResolver
+    public weak var navigationController: UINavigationController?
+    public var onFinish: (() -> Void)?
+    public var childCoordinators: [Coordinator] = []
+    public var dataBinding: DataBinding = DataBindingObject()
+    
+    public init(dependenciesResolver: DependenciesResolver, navigationController: UINavigationController?) {
+        self.dependenciesResolver = dependenciesResolver
+    }
+    
+    public func start() {
+        dependenciesResolver.resolve(for: PLHelpCenterModuleCoordinator.self).start()
+        }
+}
