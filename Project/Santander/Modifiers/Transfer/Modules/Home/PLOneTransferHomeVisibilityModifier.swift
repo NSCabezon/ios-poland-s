23e8e6579fe279c731911e3dfa908ca5949da426
Francisco Del Real Escudero <francisco.real@experis.es>
2022-03-08 22:45:49 +0100

    NEW: Added pl version visibility modifier for one transfers home.

---
+++ Project/Santander/Modifiers/Transfer/Modules/Home/PLOneTransferHomeVisibilityModifier.swift
@@ -0,0 +1,15 @@
+import Transfer
+
+struct PLOneTransferHomeVisibilityModifier: OneTransferHomeVisibilityModifier {
+    public var shouldShowFavoritesCarousel: Bool {
+        return false
+    }
+    
+    public var shouldShowNewFavoriteOption: Bool {
+        return false
+    }
+    
+    public var shouldShowRecentAndScheduledCarousel: Bool {
+        return false
+    }
+}
