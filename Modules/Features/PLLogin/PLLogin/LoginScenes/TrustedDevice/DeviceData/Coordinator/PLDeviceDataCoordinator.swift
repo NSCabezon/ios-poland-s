3652621c0d87100e5dd1d88e7bc06b1c0eb30d23
Alvaro Royo <alvaroroyo3@gmail.com>
2022-04-01 09:16:14 +0200

    Fix: 3443 Terms redirection

--- Modules/Features/PLLogin/PLLogin/LoginScenes/TrustedDevice/DeviceData/Coordinator/PLDeviceDataCoordinator.swift
+++ Modules/Features/PLLogin/PLLogin/LoginScenes/TrustedDevice/DeviceData/Coordinator/PLDeviceDataCoordinator.swift
@@ -64,7 +64,7 @@ extension PLDeviceDataCoordinator: PLTermsAndConditionsCoordinatorDelegate {
     
     func onRejectTerms() {
         let loginModuleCoordinator = self.dependenciesEngine.resolve(for: PLLoginModuleCoordinatorProtocol.self)
-        loginModuleCoordinator.loadUnrememberedLogin()
+        loginModuleCoordinator.loadUserLogin()
     }
 }
 
