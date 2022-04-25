98728575e0a25c21563e71cc6bbfd51230c64ca6
Piotr Gorzelany <188216@santander.pl>
2022-04-25 17:45:11 +0200

    FIX: Add close process button to account selection (TEET-160454)

--- Modules/Libraries/PLUI/PLUI/Components/AccountSelector/Configuration/ScreenLocationConfiguration.swift
+++ Modules/Libraries/PLUI/PLUI/Components/AccountSelector/Configuration/ScreenLocationConfiguration.swift
@@ -29,7 +29,7 @@ extension AccountSelectorViewController {
         }
         
         var showRightNavigationAction: Bool {
-            self == .mobileTransfer || self == .charityTransfer || self == .zusTransfer || self == .taxTransfer
+            self == .mobileTransfer || self == .charityTransfer || self == .zusTransfer || self == .taxTransfer || self == .phoneTopUp
         }
     }
     
