38d1b976db89163152fae15780e36d32669ddc54
Juan Carlos López Robles <>
2022-05-10 12:31:19 +0200

    ADD: Web-view opinator

--- Project/Santander/Dependencies/Modules/Opinator/OpinatorWebViewCoordinatorDependenciesResolver.swift
+++ Project/Santander/Dependencies/Modules/Opinator/OpinatorWebViewCoordinatorDependenciesResolver.swift
@@ -1,7 +1,12 @@
+import UI
 import Operative
 
 extension ModuleDependencies: OpinatorWebViewCoordinatorDependenciesResolver {
     func resolve() -> OperativeContainerCoordinatorDelegate {
         return oldResolver.resolve()
+    }
+    
+    func opinatorCoordinator() -> BindableCoordinator {
+       return OpinatorWebViewCoordinator(dependencies: self)
     }
 }
